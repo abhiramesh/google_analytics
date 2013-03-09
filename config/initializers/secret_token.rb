8941667add2259009ca8a6b1ca1c74f3055b93bb
Abhi Ramesh <abhiramesh@gmail.com>
2013-03-08 23:21:34 -0800

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Google::Application.config.secret_token = '22478c3646c440d4d43fec9915a9eb71f7e34c5b111df29fd5a17b608ef8d1eaf1e898d1455fe6561f00d305896f0cdeeebc4d31b8c805420d7c26b667b75375'
